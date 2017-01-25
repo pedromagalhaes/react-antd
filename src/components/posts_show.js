d31403acd530b1786a390e8258d9358100117dee
Pedro Magalhaes <pmagalhaes@dminc.com>
2017-01-25 14:03:20 +0700

    Delete post

--- src/components/posts_show.js
+++ src/components/posts_show.js
@@ -1,13 +1,52 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
+import { connect } from 'react-redux';
+import { fetchPost, deletePost } from '../actions/index';
+import { Link } from 'react-router';
 
 class PostsShow extends Component{
 
+    // i want access to this property from a parent component
+    static contextTypes = {
+        router: PropTypes.object
+    };
+
+    componentWillMount(){
+        // the id is coming from the url, we pass it to fetchPost which makes a BE request that resolves with some data
+        // our reducer takes it up, and make sure and we render()
+        this.props.fetchPost(this.props.params.id);
+    }
+
+    onDeleteClick(){
+        this.props.deletePost(this.props.params.id).then(() => {
+            this.context.router.push('/');
+        });
+    }
+
+
     render(){
 
-        return <div>Show post {this.props.params.id}</div>
+        const { post } = this.props;
+
+        if(!post){
+            return <div>Loading...</div>;
+        }
+
+        return (
+            <div>
+                <Link to="/">Back to Index</Link>
+                <button onClick={this.onDeleteClick.bind(this)} className="btn btn-danger pull-xs-right">Delete Post</button>
+                <h3>{post.title}</h3>
+                <h6>Categories: {post.categories}</h6>
+                <p>{post.content}</p>
+            </div>
+        );
 
     }
 
 }
 
-export default PostsShow;
+function mapStateToProps(state){
+    return { post: state.posts.post };
+}
+
+export default connect ( mapStateToProps, { fetchPost, deletePost })(PostsShow);
