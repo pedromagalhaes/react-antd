2b80574a8466de1482ecd55b4bac1481fe852181
Pedro Magalhaes <pmagalhaes@dminc.com>
2017-01-25 11:50:56 +0700

    List of posts

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -8,9 +8,19 @@ class PostsIndex extends Component {
     // "componentWillMount" is a lifecycle method
     // react calls automatically whenever the component is about to render for the first time
     // is only called once, good place to fetch our component Data
-
     componentWillMount(){
         this.props.fetchPosts();
+    }
+
+    renderPosts(){
+        return this.props.posts.map((post) => {
+            return(
+                <li className="list-group-item" key={post.id}>
+                    <span className="pull-xs-right">{post.categories}</span>
+                    <strong>{post.title}</strong>
+                </li>
+            );
+        });
     }
 
     render(){
@@ -21,13 +31,20 @@ class PostsIndex extends Component {
                         Add a post
                     </Link>
                 </div>
-                List of blog posts
+                <h3>Posts</h3>
+                <ul className="list-group">
+                    {this.renderPosts()}
+                </ul>
             </div>
         );
     }
 
 }
 
-// null because we dont have the state yet
+function mapStateToProps(state){
+    return { posts: state.posts.all }
+}
+
+// null because we don't have the state yet
 // gives access to this.props.fetchPosts
-export default connect ( null, { fetchPosts }) (PostsIndex);
+export default connect ( mapStateToProps, { fetchPosts }) (PostsIndex);
